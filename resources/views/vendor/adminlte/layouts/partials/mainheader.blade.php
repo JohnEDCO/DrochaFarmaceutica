44551825e6e9995b3ac209a703e0fe91a8859b3d
CristianDavidBernal <bernal.cristian@correounivalle.edu.co>
2019-08-02 13:12:19 -0500

    nuevas migraciones y modelos de objetivos y perspectivas

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -4,7 +4,7 @@
     <!-- Logo -->
     <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>Up</b>NE</span>
+        <span class="logo-mini"><b>D</b></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Drocha-</b>T-Virus</span>
     </a>
